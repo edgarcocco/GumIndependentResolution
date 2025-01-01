f7b3274087bd9cbf8ef132bc07d3fdcd3b62bead
Edgar V. Cocco <edgar34_@hotmail.com>
2025-01-01 12:50:19 -0500

    Updated sample code to show issue

--- src/GumIndependentResolution.Shared/GumIndependentResolutionGame.cs
+++ src/GumIndependentResolution.Shared/GumIndependentResolutionGame.cs
@@ -1,74 +1,66 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using Gum.Wireframe;
+using GumRuntime;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Audio;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Input.Touch;
 using Microsoft.Xna.Framework.Media;
+using MonoGameGum;
+using MonoGameGum.Forms;
+using MonoGameGum.Forms.Controls;
+using RenderingLibrary;
+using ResolutionBuddy;
 
 namespace GumIndependentResolution
 {
-    /// <summary>
-    /// This is the main type for your game.
-    /// </summary>
     public class GumIndependentResolutionGame : Game
     {
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
 
+        private RenderTarget2D _renderTarget;
         public GumIndependentResolutionGame()
         {
             graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
 
+            IResolution resolution = new ResolutionComponent(this, graphics,
+                                                    new Point(1024, 768),
+                                                    new Point(512, 384), false, false, false);
             graphics.SupportedOrientations = DisplayOrientation.LandscapeLeft | DisplayOrientation.LandscapeRight;
 #if (ANDROID || iOS)
             graphics.IsFullScreen = true;
 #endif
+            IsMouseVisible = true;
 
         }
 
-        /// <summary>
-        /// Allows the game to perform any initialization it needs to before starting to run.
-        /// This is where it can query for any required services and load any non-graphic
-        /// related content.  Calling base.Initialize will enumerate through any components
-        /// and initialize them as well.
-        /// </summary>
+        private GraphicalUiElement screen;
         protected override void Initialize()
         {
-            // TODO: Add your initialization logic here
+            _renderTarget = new RenderTarget2D(GraphicsDevice, 1024, 768);
+            var gumProject = MonoGameGum.GumService.Default.Initialize(
+                                       this.GraphicsDevice,
+                           "GumLayout/gumlayout.gumx");
 
+            screen = gumProject.Screens.First().ToGraphicalUiElement(
+            SystemManagers.Default, addToManagers: true);
             base.Initialize();
-
         }
 
-        /// <summary>
-        /// LoadContent will be called once per game and is the place to load
-        /// all of your content.
-        /// </summary>
         protected override void LoadContent()
         {
-            // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
-
-            // TODO: Use this.Content to load your game content here
         }
 
-        /// <summary>
-        /// UnloadContent will be called once per game and is the place to unload
-        /// game-specific content.
-        /// </summary>
         protected override void UnloadContent()
         {
-            // TODO: Unload any non ContentManager content here
         }
 
-        /// <summary>
-        /// Allows the game to run logic such as updating the world,
-        /// checking for collisions, gathering input, and playing audio.
-        /// </summary>
-        /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Update(GameTime gameTime)
         {
             MouseState mouseState = Mouse.GetState();
@@ -84,7 +76,7 @@ namespace GumIndependentResolution
             }
 
             // TODO: Add your update logic here
-
+            GumService.Default.Update(this, gameTime, screen);
             base.Update(gameTime);
         }
 
@@ -96,7 +88,12 @@ namespace GumIndependentResolution
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
-            // TODO: Add your drawing code here
+            GraphicsDevice.SetRenderTarget(_renderTarget);
+            GumService.Default.Draw();
+            GraphicsDevice.SetRenderTarget(null);
+            spriteBatch.Begin(SpriteSortMode.Immediate, BlendState.AlphaBlend, transformMatrix: ResolutionBuddy.Resolution.TransformationMatrix());
+            spriteBatch.Draw(_renderTarget, Vector2.Zero, Color.White);
+            spriteBatch.End();
 
             base.Draw(gameTime);
         }
